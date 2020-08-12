eefd7a159ffe9b191098fffc90854bd400800423
kenta.kawase <fury9jj72@gmail.com>
2020-08-12 15:19:58 +0900

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    
+  end
+  
+    private
+
+      # Strong Parameter
+      def message_params
+      params.require(:user).permit(:name,:email,:password)
+      end
+      
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |i|
+  User.create(name: "name" + i.to_s, email: "fury9jj7" + i.to_s + "@gmail.com" , password: i.to_s)
+end
